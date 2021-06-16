9c975367a2d595a062aceb99284f09e0c9eec5ca
Tony George <tony.george1984@gmail.com>
2021-06-16 16:52:12 -0400

    Initial Commit

---
+++ routes.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+from werkzeug.utils import html
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return "<h1>Welcome to Accounting Page</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
